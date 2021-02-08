f4df07908b6fd3742583bf82f7367370bd2b99ca
dina mohamed <63058966+dinamohamed85@users.noreply.github.com>
2021-02-08 01:02:45 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:layout_demo/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Example app smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(new MyApp());
+    expect(find.text('Giza Pyramid'), findsOneWidget);
+    expect(find.text('ROUTE'), findsOneWidget);
   });
 }
